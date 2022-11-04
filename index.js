20e92c3b3fd5ae50274e1479a0f2b84b338f281a
mahabubr <mahabub.r753@gmail.com>
2022-11-04 20:07:42 +0600

    jwt token done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000
 //! middleware
 app.use(cors())
 app.use(express.json())
+
+// require('crypto').randomBytes(64).toString('hex')
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+        if (error) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vlhy1ml.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,7 +52,15 @@ async function run() {
             res.send(service)
         })
 
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
+
+            const decoded = req.decoded
+            if (decoded.email !== req.query.email) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+
+            // console.log(req.headers.authorization);
+
             let query = {}
             if (req.query.email) {
                 query = {
@@ -48,7 +74,13 @@ async function run() {
 
         // Create Method
 
-        app.post('/orders', async (req, res) => {
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" })
+            res.send({ token })
+        })
+
+        app.post('/orders', verifyJWT, async (req, res) => {
             const orders = req.body
             const result = await orderCollection.insertOne(orders)
             res.send(result)
@@ -56,7 +88,7 @@ async function run() {
 
         // Update Method
 
-        app.patch('/orders/:id', async (req, res) => {
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id
             const status = req.body.status
             const query = { _id: ObjectId(id) }
@@ -71,7 +103,7 @@ async function run() {
 
         // Delete Method
 
-        app.delete('/orders/:id', async (req, res) => {
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id
             const query = {
                 _id: ObjectId(id)
